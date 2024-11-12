49b6fb260d6fec03d9f2698ddd6316ed180a94ce
roiberlin <46101561+roiberlin@users.noreply.github.com>
2024-11-12 12:11:07 +0200

    add logger import

--- android/src/main/java/com/rnbiometrics/ReactNativeBiometrics.java
+++ android/src/main/java/com/rnbiometrics/ReactNativeBiometrics.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 import android.util.Base64;
+import android.util.Log;
 
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
